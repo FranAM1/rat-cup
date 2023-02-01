0e7cd96cdb696eb6b694ba6ff8ed038499eccd40
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2023-02-01 19:42:58 +0100

    sistema de apuestas

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-let participantes = 0;
+let participantes = 2;
+let apuesta = 2;
 let campoJuego = document.getElementById("campoJuego");
 let botonIniciar = document.getElementById("botonIniciar");
 let formParticipantes = document.getElementById("formParticipantes");
-
-/*
-do {
-    participantes = prompt("Introduce el número de participantes (entre 2 y 6):");
-} while (participantes < 2 || participantes > 6);
-*/
+let intervalo;
+let dinero = localStorage.getItem("dinero");
+if (dinero == null) {
+    dinero = 100;
+    localStorage.setItem("dinero", dinero);
+}
+document.getElementById("dinero").innerHTML += `<span class="font-normal">${dinero}</span>`;
 
 function crearRatas() {
     for (let i = 0; i < participantes; i++) {
@@ -25,6 +27,7 @@ function montarJuego(){
     formParticipantes.classList.toggle("hidden");
     botonIniciar.classList.toggle("hidden");
     participantes = document.getElementById("participantes").value;
+    apuesta = document.getElementById("apuesta").value;
     
     crearRatas();
 }
@@ -33,7 +36,7 @@ function carrera(){
     let ratas = document.getElementsByClassName("rata");
     for (let i = 0; i < ratas.length; i++) {
         let posicion = ratas[i].offsetLeft;
-        let posicionQueso = document.getElementById(`queso${i}`).offsetLeft - 180;
+        let posicionQueso = document.getElementById(`queso${i}`).offsetLeft - 200;
         let random = Math.floor(Math.random() * 60) - 30;
 
         ratas[i].style.left = posicion + random + "px";
@@ -43,13 +46,23 @@ function carrera(){
 
         if (ratas[i].offsetLeft >= posicionQueso) {
             alert("La rata " + (i+1) + " ha ganado");
+            if(apuesta == i+1){
+                dinero = parseInt(dinero) + 10;
+                localStorage.setItem("dinero", dinero);
+            }else{
+                dinero = parseInt(dinero) - 10;
+                localStorage.setItem("dinero", dinero);
+            }
+            clearInterval(intervalo)
+            window.location.reload();
         }
 
     }
 }
 
 function iniciarCarrera(){
-    setInterval(carrera, 50);
+    botonIniciar.classList.toggle("hidden");
+    intervalo = setInterval(carrera, 50);
 }
 
 botonIniciar.addEventListener("click", iniciarCarrera);
