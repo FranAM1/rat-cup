5cebcac77278178a5c63da26148d11e8bb8a7c81
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2023-02-15 16:50:34 +0100

    gestionar dinero negativo + apuesta numero de rata

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rat-cup
 Carrera de ratas con js
+
+# Información sobre el proyecto
+He utilizado tailwind cdn para los estilos
+```
+<script src="https://cdn.tailwindcss.com"></script>
+```
+
+# Funcionamiento
+El juego consiste en que el usuario puede elegir entre el numero de ratas que participan en la carrera, y la rata ganadora. Según ganes o pierdas, ganarás o perderás dinero.
+
+Si te quedas en negativo no podrás seguir jugando.
+
+
--- js/main.js
+++ js/main.js
@@ -5,11 +5,18 @@ let botonIniciar = document.getElementById("botonIniciar");
 let formParticipantes = document.getElementById("formParticipantes");
 let intervalo;
 let dinero = localStorage.getItem("dinero");
+//asginar dinero inicial
 if (dinero == null) {
-    dinero = 500;
+    dinero = 200;
     localStorage.setItem("dinero", dinero);
 }
+//mostrar dinero
 document.getElementById("dinero").innerHTML += `<span class="font-normal">${dinero}</span>`;
+
+if(dinero < 0){
+    document.getElementById("ganando").classList.toggle("hidden");
+    document.getElementById("perdiendo").classList.toggle("hidden");
+}
 
 function crearRatas() {
     for (let i = 0; i < participantes; i++) {
@@ -23,15 +30,22 @@ function crearRatas() {
 }
 
 function montarJuego(){
-    campoJuego.innerHTML = "";
-    formParticipantes.classList.toggle("hidden");
-    botonIniciar.classList.toggle("hidden");
     participantes = document.getElementById("participantes").value;
     apuesta = document.getElementById("apuesta").value;
-    
-    crearRatas();
+
+    //Gestion para asegurar que apuestas por una rata que participa
+    if(apuesta > participantes){
+        alert("Has apostado por una rata que no existe, vuelve a elegir")
+    }else{
+        campoJuego.innerHTML = "";
+        formParticipantes.classList.toggle("hidden");
+        botonIniciar.classList.toggle("hidden");
+
+        crearRatas();
+    }
 }
 
+//funcion encargada de realizar los calculos para mover las ratas y ver cual gana en funcion de si su distancia es mayor o igual a la del queso
 function carrera(){
     let ratas = document.getElementsByClassName("rata");
     let dineroJuego = 10 * ratas.length
